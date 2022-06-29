186c4a77a9fb19d4d4abb7184150168f0e533985
Hernan Guardado <hernanguardado@Hernans-MacBook-Pro.local>
2022-06-29 10:51:32 -0400

    successfully scrapes yardsales and builds array from yardsalesearch.com

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const express = require('express');
 // const app = express();
 
 // YardSaleSearch (YSS)
-const YSS_URL = 'https://www.yardsalesearch.com/garage-sales.html?week=0&date=0&zip=33707&r=10&q='
-const YSS_EVENT_CLASS='.event.row'
+const week = 1
+const date = 0
+const zip = 33707
+const radius = 10
+const keyword_query = ''
+const YSS_URL = `https://www.yardsalesearch.com/garage-sales.html?week=${week}&date=${date}&zip=${zip}&r=${radius}&q=${keyword_query}`
 
 // yardsalesearch.com
 console.log('Querying ' + YSS_URL)
@@ -17,55 +21,60 @@ axios(YSS_URL)
         console.log('Parsing response...')
         const $ = cheerio.load(html)
 
-        // TODO: Modify to only pull events within search radius
-        const events = $(YSS_EVENT_CLASS, html)
-        const eventCount = events.contents().length
-        console.log(eventCount + ' event(s) found')
-
-        if (events.contents().length > 0) {
-            const sales = parseYSSSales($, events)
-        }
-        else {
-            console.log('Nothing to process...')
-        }
+        const events = $('.box.radius.clearfix > .eventList.clearFix > .event.row', html)
+        const sales = parseYSSSales($, events)
+        
     })
     .catch(error => {
         console.error(error)
     })
 
 function parseYSSSales($, events) {
-    const YSS_NAME_QUERY = 'h2[itemprop=name]'
-    // location element also includes coordinates if necessary
-    const YSS_ADDRESS_QUERY = 'div[itemprop=address]'
+    const yardsales = []
+    var count = 0
 
     events.each((index, element) => {
         const saleDetails = $(element).find('.sale-details')
 
-        const name = saleDetails.find(YSS_NAME_QUERY).text()
-        const address = saleDetails.find(YSS_ADDRESS_QUERY).text()
-        if (index < 10){
-            console.log(address)
-        }
-        // i++
+        const name = saleDetails.find('h2[itemprop=name]').text()
+        const address = saleDetails.find('div[itemprop=address]').text()
+        const latitude = saleDetails.find('meta[itemprop=latitude]').attr('content')
+        const longitude = saleDetails.find('meta[itemprop=longitude]').attr('content')
+        const startDate = saleDetails.find('meta[itemprop=startDate]').attr('content')
+        const endDate = saleDetails.find('meta[itemprop=endDate]').attr('content')
+
+        yardsales.push({
+            name,
+            address,
+            latitude,
+            longitude,
+            startDate,
+            endDate
+        })
+        count++
     })
 
+    console.log(count + ' yardsale(s) parsed')
+    return yardsales
 }
 
-function test() {
-    console.log('Parsing ');
-    // const list = document.getElementsByClassName('event row');
-    // console.log(list);
-    // console.log(list.length + ' rows retrieved');
 
-    // list.forEach(parseAddress);
 
-    // const item = list[0];
-    // console.log(item.textContent);
-}
+// function test() {
+//     console.log('Parsing ');
+//     const list = document.getElementsByClassName('event row');
+//     console.log(list);
+//     console.log(list.length + ' rows retrieved');
 
-function parseAddress(row, index) {
-    if (index==0) {
-        console.log(row.textContent);
-    }
-}
+//     list.forEach(parseAddress);
+
+//     const item = list[0];
+//     console.log(item.textContent);
+// }
+
+// function parseAddress(row, index) {
+//     if (index==0) {
+//         console.log(row.textContent);
+//     }
+// }
 
