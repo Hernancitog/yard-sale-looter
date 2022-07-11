c5878099010cb7e50ca6ed0daf38441f863c35bc
Hernan Guardado <hernanguardado@hernans-mbp.lan>
2022-07-11 14:56:57 -0400

    adding .env property resolution

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
-const path = require('path');
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new Dotenv()
+        // new webpack.DefinePlugin({
+        //     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+        // })
+    ]
 };
